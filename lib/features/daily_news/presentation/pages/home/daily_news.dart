fafb0f7d6a1a9d493cdc2f8df224c4ab9f825323
Muhammet Colak <muhammetcolak@hotmail.com>
2024-11-10 14:02:39 +0100

    - Video 10 - Home Widdget design - ! occur error: Exception has occurred. _CastError (Null check operator used on a null value) -> !article

--- lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clean_architecture_news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:flutter_clean_architecture_news_app/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+import 'package:flutter_clean_architecture_news_app/features/daily_news/presentation/widgets/article_tile.dart';
 
 
 class DailyNews extends StatelessWidget {
@@ -39,8 +40,8 @@ _buildBody() {
       if (state is RemoteArticlesDone) {
         return ListView.builder(
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('$index'),             
+            return ArticleWidget(
+              article: state.articles![index],             
             );
           },
           itemCount: state.articles!.length,
